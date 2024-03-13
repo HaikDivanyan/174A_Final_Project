c720cfb45ea8d4c85c48428081ad4b45f5f606c2
Akash <abanerjee0047@gmail.com>
2024-03-13 12:07:31 -0700

    refractored main scene

--- main-scene.js
+++ main-scene.js
@@ -1,7 +1,5 @@
 import { defs, tiny } from "./common.js";
 import { SupermanSimGame } from "./supermanSim.js";
-
-// Pull these names into this module's scope for convenience:
 const {
   Vector,
   Vector3,
@@ -21,21 +19,6 @@ const {
   Code_Widget,
   Text_Widget,
 } = tiny;
-
-// Now we have loaded everything in the files tiny-graphics.js, tiny-graphics-widgets.js, and common.js.
-// This yielded "tiny", an object wrapping the stuff in the first two files, and "defs" for wrapping all the rest.
-
-// ******************** Extra step only for when executing on a local machine:
-//                      Load any more files in your directory and copy them into "defs."
-//                      (On the web, a server should instead just pack all these as well
-//                      as common.js into one file for you, such as "dependencies.js")
-
 Object.assign(defs, { SupermanSimGame: SupermanSimGame });
-
-// ******************** End extra step
-
-// (Can define Main_Scene's class here)
-
 const Main_Scene = SupermanSimGame;
-
 export { Canvas_Widget, Code_Widget, Main_Scene, Text_Widget, defs };
